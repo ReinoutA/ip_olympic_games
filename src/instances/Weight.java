6543259e166ae4d622004d4148701b6df9d48d8e
Reinout Annaert <reinout.annaert@student.kuleuven.be>
2023-10-30 10:57:26 +0100

    added weight instance

---
+++ src/instances/Weight.java
@@ -0,0 +1,27 @@
+package instances;
+
+public class Weight {
+    private String name;
+    private double weight;
+
+    public Weight(String name, double weight){
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double getWeight(){
+        return weight;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setWeight(double weight){
+        this.weight = weight;
+    }
+}
